7902b216ee9768af68d3aa2b24ffd2d532280682
Cebreus <cebreus@live.com>
2022-10-15 10:04:13 +0200

    refactor: replace version in `faviconGenConfig`

--- gulpconfig.build.js
+++ gulpconfig.build.js
@@ -99,7 +99,7 @@ const faviconGenConfig = {
   orientation: 'portrait-primary',
   scope: '/',
   start_url: '/index.html',
-  version: 1.0,
+  version: '0.0.0',
   logging: false,
   html: 'favicons.njk',
   pipeHTML: true,
