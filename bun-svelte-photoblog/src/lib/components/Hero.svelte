5acc06d016eb73e39defb4d7701b082670454975
Jaroslav Cebreus Vrana <cebreus@live.com>
2025-12-06 19:53:03 +0100

    fix(Hero): update background colors and text styles for consistency

--- bun-svelte-photoblog/src/lib/components/Hero.svelte
+++ bun-svelte-photoblog/src/lib/components/Hero.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <section
-  class="py-24 _bg-linear-to-br dark:from-cblue-800 dark:to-cblue-950 from-cblue-100 to-cblue-200"
+  class="py-24 bg-linear-to-br from-slate-700 to-slate-950"
   data-testid="hero-section"
 >
   <div class="grid items-center mx-auto container gap-8 lg:grid-cols-1">
@@ -19,18 +19,111 @@
         </Badge> -->
 
       <h1
-        class="my-6 text-pretty text-3xl font-bold lg:text-5xl"
+        class="my-6 text-pretty text-3xl font-bold lg:text-5xl text-transparent bg-clip-text bg-gradient-to-br from-orange-400 to-red-600"
         data-testid="hero-title"
       >
         Izrael 2022
       </h1>
 
       <p
-        class="text-muted-foreground mb-8 max-w-xl lg:text-lg text-balance"
+        class="text-slate-300 mb-8 max-w-xl lg:text-lg text-balance"
         data-testid="hero-description"
       >
         Fotografické střípky z výletu do Izraele a Jordánska.
       </p>
+
+      <!-- <div
+        class="flex gap-3 justify-center items-center overflow-x-auto whitespace-nowrap"
+      >
+        <div
+          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-slate-50"
+        >
+          <span
+            class="text-xs font-semibold text-slate-800 bg-white/40 rounded px-1"
+            >slate-50</span
+          >
+        </div>
+        <div
+          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-slate-100"
+        >
+          <span
+            class="text-xs font-semibold text-slate-800 bg-white/40 rounded px-1"
+            >slate-100</span
+          >
+        </div>
+        <div
+          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-slate-200"
+        >
+          <span
+            class="text-xs font-semibold text-slate-800 bg-white/40 rounded px-1"
+            >slate-200</span
+          >
+        </div>
+        <div
+          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-slate-300"
+        >
+          <span
+            class="text-xs font-semibold text-slate-800 bg-white/40 rounded px-1"
+            >slate-300</span
+          >
+        </div>
+        <div
+          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-slate-400"
+        >
+          <span
+            class="text-xs font-semibold text-slate-800 bg-white/40 rounded px-1"
+            >slate-400</span
+          >
+        </div>
+        <div
+          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-slate-500"
+        >
+          <span
+            class="text-xs font-semibold text-white bg-white/10 rounded px-1"
+            >slate-500</span
+          >
+        </div>
+        <div
+          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-slate-600"
+        >
+          <span
+            class="text-xs font-semibold text-white bg-white/10 rounded px-1"
+            >slate-600</span
+          >
+        </div>
+        <div
+          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-slate-700"
+        >
+          <span
+            class="text-xs font-semibold text-white bg-white/10 rounded px-1"
+            >slate-700</span
+          >
+        </div>
+        <div
+          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-slate-800"
+        >
+          <span
+            class="text-xs font-semibold text-white bg-white/10 rounded px-1"
+            >slate-800</span
+          >
+        </div>
+        <div
+          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-slate-900"
+        >
+          <span
+            class="text-xs font-semibold text-white bg-white/10 rounded px-1"
+            >slate-900</span
+          >
+        </div>
+        <div
+          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-slate-950"
+        >
+          <span
+            class="text-xs font-semibold text-white bg-white/10 rounded px-1"
+            >slate-950</span
+          >
+        </div>
+      </div>
 
       <div
         class="flex gap-3 justify-center items-center overflow-x-auto whitespace-nowrap"
@@ -129,99 +222,6 @@
         class="flex gap-3 justify-center items-center overflow-x-auto whitespace-nowrap"
       >
         <div
-          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-cblue-50"
-        >
-          <span
-            class="text-xs font-semibold text-slate-800 bg-white/40 rounded px-1"
-            >cblue-50</span
-          >
-        </div>
-        <div
-          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-cblue-100"
-        >
-          <span
-            class="text-xs font-semibold text-slate-800 bg-white/40 rounded px-1"
-            >cblue-100</span
-          >
-        </div>
-        <div
-          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-cblue-200"
-        >
-          <span
-            class="text-xs font-semibold text-slate-800 bg-white/40 rounded px-1"
-            >cblue-200</span
-          >
-        </div>
-        <div
-          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-cblue-300"
-        >
-          <span
-            class="text-xs font-semibold text-slate-800 bg-white/40 rounded px-1"
-            >cblue-300</span
-          >
-        </div>
-        <div
-          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-cblue-400"
-        >
-          <span
-            class="text-xs font-semibold text-slate-800 bg-white/40 rounded px-1"
-            >cblue-400</span
-          >
-        </div>
-        <div
-          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-cblue-500"
-        >
-          <span
-            class="text-xs font-semibold text-white bg-white/10 rounded px-1"
-            >cblue-500</span
-          >
-        </div>
-        <div
-          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-cblue-600"
-        >
-          <span
-            class="text-xs font-semibold text-white bg-white/10 rounded px-1"
-            >cblue-600</span
-          >
-        </div>
-        <div
-          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-cblue-700"
-        >
-          <span
-            class="text-xs font-semibold text-white bg-white/10 rounded px-1"
-            >cblue-700</span
-          >
-        </div>
-        <div
-          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-cblue-800"
-        >
-          <span
-            class="text-xs font-semibold text-white bg-white/10 rounded px-1"
-            >cblue-800</span
-          >
-        </div>
-        <div
-          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-cblue-900"
-        >
-          <span
-            class="text-xs font-semibold text-white bg-white/10 rounded px-1"
-            >cblue-900</span
-          >
-        </div>
-        <div
-          class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-cblue-950"
-        >
-          <span
-            class="text-xs font-semibold text-white bg-white/10 rounded px-1"
-            >cblue-950</span
-          >
-        </div>
-      </div>
-
-      <div
-        class="flex gap-3 justify-center items-center overflow-x-auto whitespace-nowrap"
-      >
-        <div
           class="w-28 h-16 rounded-lg flex items-end justify-center p-2 bg-blue-50"
         >
           <span
@@ -309,7 +309,7 @@
             >blue-950</span
           >
         </div>
-      </div>
+      </div> -->
 
       <!-- <div
           class="flex w-full flex-col justify-center gap-2 sm:flex-row lg:justify-start"
