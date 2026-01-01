c8aa908db13a31fafb2cee7cea6f6b69cb45a938
Jaroslav Cebreus Vrana <cebreus@live.com>
2026-01-01 16:44:12 +0100

    style: beautify

--- src/lib/components/sidebar-content/EditTab.svelte
+++ src/lib/components/sidebar-content/EditTab.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
   import LayoutGrid from "@lucide/svelte/icons/layout-grid";
-  import { toast } from "svelte-sonner";
   import { fade } from "svelte/transition";
-
-  import MetadataPasteDialog from "$lib/components/MetadataPasteDialog.svelte";
+  import { toast } from "svelte-sonner";
+  import { invalidateAll } from "$app/navigation";
   import CollageDialog from "$lib/components/admin/CollageDialog.svelte";
+  import MetadataPasteDialog from "$lib/components/MetadataPasteDialog.svelte";
   import { Button } from "$lib/components/ui/button";
   import { Label } from "$lib/components/ui/label";
   import { Spinner } from "$lib/components/ui/spinner";
@@ -22,8 +22,6 @@
   } from "$lib/utils/collage-config";
   import { COLLAGE_MESSAGES, IMAGE_MESSAGES } from "$lib/utils/messages";
   import { smartToast } from "$lib/utils/toasts";
-
-  import { invalidateAll } from "$app/navigation";
 
   import GeoDataSection from "./GeoDataSection.svelte";
   import MetadataInputField from "./MetadataInputField.svelte";
