020332f2311f1e5ef2a6c9e91c80b84af8c45ce3
Jaroslav Cebreus Vrana <cebreus@live.com>
2026-01-14 10:52:07 +0100

    fix: imports

--- src/lib/components/ui/accordion/accordion-content.svelte
+++ src/lib/components/ui/accordion/accordion-content.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { Accordion as AccordionPrimitive } from "bits-ui";
 
-  import { type WithoutChild, cn } from "$lib/utils/index.js";
+  import { type WithoutChild, cn } from "$lib/utils";
 
   let {
     ref = $bindable(null),
